b34bb08b422b3c34e88a7dd7223e433169ccc1e2
mathmoul <mathieu.moullec@gmail.com>
2017-03-28 18:28:01 +0200

    ll

--- scripting/js/script.js
+++ scripting/js/script.js
@@ -1,30 +1,29 @@
 
 var cadre = document.querySelector('.cadre');
 var masques = document.querySelectorAll('.m');
-console.log(masques);
 
+var selected = null;
 var canvas = document.querySelector('.canvas');
 var context = canvas.getContext('2d');
 
 for (var i= 0; i < masques.length; i++) {
     masque = masques[i];
     var test = function (e) {
-        if (this.getAttribute('style')) {
-            alert('objet deja selectionne');
-            e.preventDefault();
+        if (!selected) {
+            selected = this;
+            selected.setAttribute('style', 'opacity: 0.5');
         }
-        this.setAttribute('style', 'opacity: 0.5');
-        console.log(this);
+        else {
+            selected.removeAttribute('style');
+            if (selected === this) {
+                alert('Vous ne pouvez pas selectionner le meme objet!!!!!');
+            }
+            selected = this;
+            selected.setAttribute('style', 'opacity: 0.5');
+        }
         e.stopPropagation();
-        this.removeEventListener('click', test);
+        context.drawImage('selected', 0, 0, 10, 10);
     }
-    console.log(masque.addEventListener('click', test));
+    masque.addEventListener('click', test);
 }
 
-window.addEventListener('click', function () {
-    for (var i= 0; i < masques.length; i++) {
-        masque = masques[i];
-        masque.removeAttribute('style');
-        console.log(masque);
-    }
-})
